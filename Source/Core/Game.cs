e73c2a99a85e8c41b473df44cb5850198922a9ab
L33tCr33p3r <coryvin@thebandit.org>
2023-03-13 18:17:37 -0600

    moved Game.cs to Core folder and added ECS dictionary

--- Source/Core/Game.cs
+++ Source/Core/Game.cs
@@ -1,7 +1,9 @@
-namespace Not_Celeste;
+namespace Not_Celeste.Core;
 
 internal class Game
 {
+	public Dictionary<Type, List<IComponent>> ECS;
+
 	public void Run()
 	{
 		Console.WriteLine("Hello!");
