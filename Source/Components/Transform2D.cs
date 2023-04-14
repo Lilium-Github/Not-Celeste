ed725f1268720740a8527e09b02ba3afe492aa1a
L33tCr333pr <coryvin@thebandit.org>
2023-04-14 10:06:11 -0600

    Transform2D now supports + and - arithmetic operators.

--- Source/Components/Transform2D.cs
+++ Source/Components/Transform2D.cs
@@ -1,5 +1,26 @@
 namespace Not_Celeste.Components;
 
 internal class Transform2D : Component {
-	public Vector2f Position { get; set; }
+	public required Vector2f Position { get; set; }
+
+	public Transform2D Copy() {
+		var targPos = Position;
+		return new Transform2D() {
+			Position = targPos
+		};
+	}
+
+	public static Transform2D operator +(Transform2D a, Transform2D b) {
+		return new Transform2D()
+		{
+			Position = a.Position + b.Position
+		};
+	}
+
+	public static Transform2D operator -(Transform2D a, Transform2D b) {
+		return new Transform2D()
+		{
+			Position = a.Position - b.Position
+		};
+	}
 }
