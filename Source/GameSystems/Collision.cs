0d86077bf57b869c0565485846ae18119d3fb40e
L33tCr333pr <coryvin@thebandit.org>
2023-04-27 16:54:43 -0600

    Finished (probably) the Collision system

--- Source/GameSystems/Collision.cs
+++ Source/GameSystems/Collision.cs
@@ -3,7 +3,34 @@ namespace Not_Celeste.GameSystems;
 internal class Collision : GameSystem {
 	public override void Process(double Delta) {
 		foreach (GameObject gameObject in SceneTree.GetGameObjects(new[] { typeof(Transform2D), typeof(CollisionArea) })) {
-			
+			var GlobalTransform = Transform2DUtils.GetGlobalTransform2D(gameObject);
+			foreach (GameObject gameObject2 in SceneTree.GetGameObjects(new[] { typeof(Transform2D), typeof(CollisionArea) })) {
+				var GlobalTransform2 = Transform2DUtils.GetGlobalTransform2D(gameObject);
+
+				var isCollided = false;
+				foreach (FloatRect collisionRect in ((CollisionArea) gameObject[typeof(CollisionArea)]).Bounds) {
+					foreach (FloatRect collisionRect2 in ((CollisionArea) gameObject[typeof(CollisionArea)]).Bounds) {
+						if (
+							new FloatRect(
+								collisionRect.Left + GlobalTransform.Position.X,
+								collisionRect.Top + GlobalTransform.Position.Y,
+								collisionRect.Width,
+								collisionRect.Height
+							).Intersects(
+								new FloatRect(
+									collisionRect2.Left + GlobalTransform2.Position.X,
+									collisionRect2.Top + GlobalTransform2.Position.Y,
+									collisionRect2.Width,
+									collisionRect2.Height
+								)
+							)
+						) isCollided = true;
+					}
+				}
+				if (isCollided) {
+					((CollisionArea) gameObject[typeof(CollisionArea)]).CollidedObjects.Add(gameObject2);
+				}
+			}
 		}
 	}
 }
