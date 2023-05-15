e79216764699d38fe0b8915c5304f0b98e3471b9
L33tCr333pr <coryvin@thebandit.org>
2023-05-15 14:52:52 -0600

    almost done with polygon projection collision

--- Source/Utils/CollisionShape.cs
+++ Source/Utils/CollisionShape.cs
@@ -6,16 +6,16 @@ internal abstract class CollisionShape {
 	/// 
 	/// </summary>
 	/// <returns></returns>
-	public abstract List<Vector2f> getPolygon();
+	public abstract List<Vector2f> GetPolygon();
 
 	// public bool collide(CollisionShape target) {
 
 	// }
 	
-	public Tuple<double, Line>? ProjectionCollide(CollisionShape target, Vector2f Velocity)
+	public Tuple<double, Line>? ProjectionCollide(CollisionShape target, Vector2f velocity)
 	{
-		var poly1 = getPolygon();
-		var poly2 = target.getPolygon();
+		var poly1 = GetPolygon();
+		var poly2 = target.GetPolygon();
 
 		var lines1 = new List<Line>(poly1.Count);
 		var lines2 = new List<Line>(poly2.Count);
@@ -28,16 +28,37 @@ internal abstract class CollisionShape {
 			lines2.Add(new(poly2[i], poly2[i + 1 % poly2.Count]));
 		}
 
-		var collisions = new List<Tuple<double, Tuple<Vector2f, Vector2f>>>();
+		var collisions = new List<Tuple<double, Line>>();
 
 		foreach (var point in poly1) {
 			foreach (var line in lines2) {
-				// collide the line made by (point, point + Velocity) with line
-				
-				
+				var collidePoint = new Line(point, point + velocity).Collide(line);
 
+				if (collidePoint != null) {
+					var distanceVector = (Vector2f) collidePoint - point;
+
+					var distanceSquared = Math.Pow(distanceVector.X, 2) + Math.Pow(distanceVector.Y, 2);
+
+					collisions.Add(new(distanceSquared, line));
+				}
 			}
 		}
+
+		foreach (var point in poly2) {
+			foreach (var line in lines1) {
+				var collidePoint = new Line(point, point - velocity).Collide(line);
+
+				if (collidePoint != null) {
+					var distanceVector = (Vector2f) collidePoint - point;
+
+					var distanceSquared = Math.Pow(distanceVector.X, 2) + Math.Pow(distanceVector.Y, 2);
+
+					collisions.Add(new(distanceSquared, line));
+				}
+			}
+		}
+
+
 		return null;
 	}
 }
